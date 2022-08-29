40973af05e5ac99aca36be686783c12d8fc7abdf
pr57039n <107935666+pr57039n@users.noreply.github.com>
2022-08-29 13:48:45 -0400

    Create Hydration.py

---
+++ Hydration.py
@@ -0,0 +1,5 @@
+# Per hour; 0.5 liters of water should be consumed. Rounding down to the nearest integer. So 1 hour would = 0 litres.
+
+def litres(time):
+    water = 0.5 * time
+    return int(water)
