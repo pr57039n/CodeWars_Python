2055fe926d5fe8436a89de080867f55ba07f0aaa
pr57039n <107935666+pr57039n@users.noreply.github.com>
2023-02-22 17:16:59 -0500

    Update passwordrequirements.py

--- passwordrequirements.py
+++ passwordrequirements.py
@@ -1,5 +1,8 @@
+# Password Requirements would need 1 number, and at least 7 characters.
+# Changed to just make additional requirements use an and statement
+
 def is_acceptable_password(password: str) -> bool:
-    if len(password) < 7:
-        return False
-    else:
+    if len(password) > 7 and any(map(str.isdigit, password)):
         return True
+    else:
+        return False
